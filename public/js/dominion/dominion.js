e00dbb196906a6a77640df5efbf75ce841c4bbff
Geordan Rosario <geordan@gmail.com>
2010-08-14 10:23:06 -0700

    SVGs from Dawn.

--- public/js/dominion/dominion.js
+++ public/js/dominion/dominion.js
@@ -39,7 +39,8 @@ function update_spread(refresh) {
       var inac = cardbuf.getInactive();
       spread.forEach(function(card) {
         var id = card['_id'];
-        inac.append('<li><a id="'+id+'" name="'+card.name+'">'+card.name+'<small><img src="/images/dominion/'+card.source+'.png" alt="'+card.source+'"/></small></a></li>');
+        var img = card.source == 'boardgamegeek' ? 'ferris-wheel' : card.source.toLowerCase();
+        inac.append('<li><a id="'+id+'" name="'+card.name+'">'+card.name+'<small><img src="/images/dominion/'+img+'.svg" width="20" height="20" alt="'+card.source+'"/></small></a></li>');
       });
 
       cardbuf.swap();
